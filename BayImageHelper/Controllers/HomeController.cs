db1e5d429b55298a3d921587fe25c7de50e03a52
messagecompass <messagecompass@gmail.com>
2014-10-08 15:27:09 -0400

    Add get image function to avoid blockage for directly refering in img tag

--- BayImageHelper/Controllers/HomeController.cs
+++ BayImageHelper/Controllers/HomeController.cs
@@ -1,20 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace BayImageHelper.Controllers
 {
+
+
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult Get(string url)
+        {
+            //string imgurl = "http://image.bayimg.com/38ba4e167a8f6ee9dc800b604cb0cfc6663bd720.jpg";
+            var wc = new System.Net.WebClient();
+            var imageData = wc.DownloadData(url);
+            return File(imageData, "image/png"); // Might need to adjust the content type based on your actual image type
+        }
+
+        [HttpPost]
+        public JsonResult Upload(HttpPostedFileBase file)
+        {
+            var t = Request.Files[0] as HttpPostedFileBase;
+            var img = new BayImage.Models.BayimgClient();
+            return Json(img.POSTBayImage(t, t.FileName));
+        }
         [HttpPost]
         public string Index(HttpPostedFileBase file)
         {
+            return "ok";
+
             if (file != null && file.ContentLength > 0)
             {
                 var img = new BayImage.Models.BayimgClient();
